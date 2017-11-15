6cec962d79348ab4f4cc69f75fd8adb53a02e60b
sebasrojas17 <you@example.com>
2017-11-15 17:27:32 -0500

    Sofia

---
+++ client/app/app.constant.js
@@ -0,0 +1,13 @@
+(function(angular, undefined) {
+  angular.module("sofiaplusApp.constants", [])
+
+.constant("appConfig", {
+	"userRoles": [
+		"guest",
+		"user",
+		"admin"
+	]
+})
+
+;
+})(angular);
